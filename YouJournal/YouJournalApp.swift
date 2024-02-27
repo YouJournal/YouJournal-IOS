ec23292e61d3aa0d228b4b9f463e2a5fa7e8b942
Dexify <info@dexify.io>
2024-02-27 16:55:54 +0000

    Initial Commit

---
+++ YouJournal/YouJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouJournalApp.swift
+//  YouJournal
+//
+//  Created by Luke Trotman on 27/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct YouJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
